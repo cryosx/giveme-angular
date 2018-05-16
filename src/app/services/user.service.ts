286285b8b422460e33d035abb69d14464fbe0378
cryosx <cyruswu.email@gmail.com>
2018-05-16 13:50:28 -1000

    added login function

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AuthenticateService } from './authenticate.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,19 +8,40 @@ import { Injectable } from '@angular/core';
 export class UserSerivce {
   isLoggedIn: boolean;
   user: Object;
-  constructor() {
-    this.user = localStorage.getItem('user');
+  constructor(private authService: AuthenticateService) {
+    this.user = JSON.parse(localStorage.getItem('user'));
     this.isLoggedIn = this.user ? true : false;
   }
 
+  login(data) {
+    return this.authService.login(data)
+      .toPromise()
+      .then(user => {
+        console.log(user);
+        localStorage.setItem('user', JSON.stringify(user));
+        return this.isLoggedIn = true;
+      }).catch(err => {
+        console.log(err);
+      });
+  }
+
   logout() {
-    return this.isLoggedIn = false;
+    return this.authService.logout()
+      .toPromise()
+      .then(data => {
+        localStorage.removeItem('user');
+        return this.isLoggedIn = false;
+      }).catch(err => {
+        console.log(err);
+      });
   }
 
   getUser() {
     return this.user;
   }
-  getIsLoggedIn() {
+
+  getIsLoggedIn = () => {
+    // console.log(this);
     return this.isLoggedIn;
   }
 }
