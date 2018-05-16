2a9dad672867147efa4e4767431ecdd5cceb23f7
cryosx <cyruswu.email@gmail.com>
2018-05-16 13:50:11 -1000

    removed login/register service and combined into authenticate service

---
+++ src/app/services/authenticate.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthenticateService {
+  constructor(private http: HttpClient) { }
+  // submitInquiry(data) {
+  //   return this.http.post('http://localhost:3000', data);
+  // }
+  login(data) {
+    // return this.http.get('http://localhost:3000/api/task');
+    console.log('login', data);
+    return this.http.post('/api/login', data);
+  }
+  logout() {
+    return this.http.get('/api/logout');
+  }
+  register(data) {
+    // return this.http.get('http://localhost:3000/api/task');
+    console.log('register', data);
+    return this.http.post('/api/register', data).toPromise()
+      .then(user => {
+        console.log(user);
+        localStorage.setItem('user', JSON.stringify(user));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+}
