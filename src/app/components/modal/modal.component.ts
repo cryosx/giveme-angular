a39c13becd4669a87f584bd74e2a94ffe1618a33
cryosx <cyruswu.email@gmail.com>
2018-05-16 10:20:22 -1000

    prepped modals

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent {
+
+}
