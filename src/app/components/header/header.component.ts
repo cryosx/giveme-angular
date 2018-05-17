dbcfbba16bc3686263df1cb8507634b9f5dd9ee5
cryosx <cyruswu.email@gmail.com>
2018-05-16 15:59:47 -1000

    logout button/link

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserSerivce } from '../../services/user.service';
 
 
 @Component({
@@ -7,6 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isLoggedIn;
 
+  constructor(private user: UserSerivce, private router: Router) {
+    this.isLoggedIn = user.getIsLoggedIn;
+  }
+  logout() {
+    this.user.logout();
+    this.router.navigateByUrl('/');
+  }
 
 }
