02e87e3ac9af34fbb5aa352be37871741cf08f3a
cryosx <cyruswu.email@gmail.com>
2018-05-18 12:24:42 -1000

    user-tasks compnent mostly implemented

--- src/app/components/task/user-tasks/user-tasks.component.ts
+++ src/app/components/task/user-tasks/user-tasks.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskSerivce } from '../../../services/task.service';
+import { UserSerivce } from '../../../services/user.service';
 
 @Component({
   selector: 'app-user-tasks',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-tasks.component.scss']
 })
 export class UserTasksComponent implements OnInit {
+  cookieUser: Object;
+  sessionUser: Object;
+  userTasks: any;
+  userActiveTasks: any;
 
-  constructor() { }
+  constructor(
+    private taskService: TaskSerivce,
+    private userService: UserSerivce
+  ) {
+    this.cookieUser = {};
+    this.sessionUser = {};
 
-  ngOnInit() {
+    this.userTasks = [];
+    this.userActiveTasks = [];
   }
 
+  ngOnInit() {
+    this.getUserTasks();
+  }
+
+  getUserTasks() {
+    this.cookieUser = this.userService.getUser();
+    const id = this.cookieUser['id'];
+    this.taskService
+      .getUserTasks(id)
+      .toPromise()
+      .then(user => {
+        this.sessionUser = user;
+        this.userTasks = user['myTasks'];
+        this.userActiveTasks = user['activeTasks'];
+        console.log(this.userTasks);
+        console.log(this.userActiveTasks);
+      })
+      .catch();
+  }
 }
